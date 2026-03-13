c19c475913c24668229de4fed6ae81991762d3c9
Anshika Thakur <at1498724@gmail.com>
2026-03-13 09:49:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+model=pickle.load(open('tips_model.pkl','rb'))
+
+st.title("Tip Prediction App")
+
+total_bill=st.number_input("Total Bill")
+size=st.number_input("size")
+sex=st.selectbox("Sex",['Male','Female'])
+smoker=st.selectbox("Smoker",['Yes','No'])
+day=st.selectbox("Day",['Thur','Fri','Sat','Sun'])
+time=st.selectbox("Time",['Lunch','Dinner'])
+
+input_data=pd.DataFrame({
+    'total_bill':[total_bill],
+    'size':[size],
+    'sex':[sex],
+    'smoker':[smoker],
+    'day':[day],
+    'time':[time]
+})
+if st.button("Predict Tip"):
+    prediction=model.predict(input_data)
+    st.success(f"predicted Tip:{prediction[0]:.2f}") 
